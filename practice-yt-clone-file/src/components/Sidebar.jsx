401e8d29f1a96619f2a99fb1549d0687a553c1ad
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-21 17:10:19 +0700

    update index.css / create sidebar css

--- practice-yt-clone-file/src/components/Sidebar.jsx
+++ practice-yt-clone-file/src/components/Sidebar.jsx
@@ -1,7 +1,16 @@
 
 function Sidebar() {
   return (
-    <section id="sidebar">Sidebar</section>
+    <section id="side">
+        <div className="main-side">
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">หน้าแรก</div>
+            </a>
+        </div>
+    </section>
   )
 }
 
