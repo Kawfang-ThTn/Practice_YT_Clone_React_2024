754d204762662b16eaf0f180bf6bcbb2e482d7c9
KawfangThTn <thanthong.somwijit@gmail.com>
2024-10-22 02:56:33 +0700

    create tags / contents style

--- practice-yt-clone-file/src/components/Contents.jsx
+++ practice-yt-clone-file/src/components/Contents.jsx
@@ -1,7 +1,39 @@
+import Tags from './Tags.jsx'
+import './ContentsStyle.css'
+
 
 function Contents() {
   return (
-    <section id="content">Contents</section>
+    <section id="content">
+      <Tags />
+
+      <section id="tag-card">
+
+        <div className="card">
+
+          <div className="thumbnail">
+            <img src="https://via.placeholder.com/480x270" alt="" />
+          </div>
+
+          <div className="about">
+
+            <div className="chanel-img">
+              <img src="https://via.placeholder.com/36x36" alt="" />
+            </div>
+
+            <div className="chanel-detail"></div>
+
+              <div className="detail-title">ทดลองโคลนนิ่งยูทูป</div>
+              <div className="detail-name">Kawfang-ThTn</div>
+              <div className="detail-view">การดู 44.44 ล้านครั้ง • 4 วันที่ผ่านมา</div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+    </section>
   )
 }
 
--- practice-yt-clone-file/src/components/Tags.jsx
+++ practice-yt-clone-file/src/components/Tags.jsx
@@ -1,7 +1,22 @@
+import './TagsStyle.css';
 
 function Tags() {
   return (
-    <div>Tags</div>
+    <section id="tag">
+
+      <a href="" className="tag-item active">
+        ทั้งหมด
+      </a>  
+
+      <a href="" className="tag-item">
+        ทั้งหมด
+      </a>  
+
+      <a href="" className="tag-item">
+        ทั้งหมด
+      </a>  
+
+    </section>
   )
 }
 
