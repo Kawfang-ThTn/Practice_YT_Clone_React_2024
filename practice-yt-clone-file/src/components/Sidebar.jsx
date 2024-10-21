92affb093fffa84622fff45cc57d5a9c0815a6c1
KawfangThTn <thanthong.somwijit@gmail.com>
2024-10-22 02:10:11 +0700

    sidebar mock-up complete v1

--- practice-yt-clone-file/src/components/Sidebar.jsx
+++ practice-yt-clone-file/src/components/Sidebar.jsx
@@ -1,14 +1,206 @@
+import './SidebarStyle.css';
 
 function Sidebar() {
   return (
     <section id="side">
         <div className="main-side">
+
             <a href="#" className="side-item">
                 <div className="side-icon">
                     <img src="https://via.placeholder.com/24x24" alt="" />
                 </div>
                 <div className="side-text">หน้าแรก</div>
             </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">Shorts</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
+            <a href="#" className="side-item">
+                <div className="side-icon">
+                    <img src="https://via.placeholder.com/24x24" alt="" />
+                </div>
+                <div className="side-text">การติดตาม</div>
+            </a>
+
         </div>
     </section>
   )
