9f52690d8a5f50288b4c156d321fc9addfad1d99
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-21 16:30:21 +0700

    nav search-input

--- practice-yt-clone-file/src/components/Nav.jsx
+++ practice-yt-clone-file/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import "./NavStyle.css";
 function Nav() {
   return (
     <section id="nav">
+
         <div className="bar-logo">
             <div className="hamburger-bar">
                 <img src="https://via.placeholder.com/24x24" alt="" />
@@ -11,8 +12,23 @@ function Nav() {
             <img src="https://via.placeholder.com/97x20" alt="" />
             </div>
         </div>
-        <div className="search-area">2</div>
-        <div className="user-profile">3</div>
+
+        <div className="center">
+            <div className="search-area">
+                <div className="search-box">
+                    <input type="text" className="search-input" />
+                </div>
+                <div className="search-btn">Q</div>
+            </div>
+            
+            <div className="voice-btn"></div>
+        </div>
+
+        <div className="user-menu">
+            <div className="noti-menu">x</div>
+            <div className="noti-menu">x</div>
+            <div className="noti-menu">x</div>
+        </div>
     </section>
   )
 }
