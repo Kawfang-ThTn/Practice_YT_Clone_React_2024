0bce4925432b4343db30bb40f6f57f742a7bd89d
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-22 15:28:17 +0700

    mock-up content/short complete v1

--- practice-yt-clone-file/src/components/Contents.jsx
+++ practice-yt-clone-file/src/components/Contents.jsx
@@ -129,10 +129,78 @@ function Contents() {
 
         </div>
 
-        
-        
-
       </section>
+
+      <section id="short">
+          <div className="short-title"> [] Shorts</div>
+
+          <div className="short-wrap">
+
+            <div className="short">
+              <div className="short-thumb">
+                <img src="https://via.placeholder.com/270x480" alt="" />
+              </div>
+              <div className="short-about">
+                <div className="short-detail">
+                  <a href="" className="short-name">ทดลองโคลนนิ่งยูทูป</a>
+                  <div className="short-view">การดู 44.44 ล้านครั้ง</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="short">
+              <div className="short-thumb">
+                <img src="https://via.placeholder.com/270x480" alt="" />
+              </div>
+              <div className="short-about">
+                <div className="short-detail">
+                  <a href="" className="short-name">ทดลองโคลนนิ่งยูทูป</a>
+                  <div className="short-view">การดู 44.44 ล้านครั้ง</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="short">
+              <div className="short-thumb">
+                <img src="https://via.placeholder.com/270x480" alt="" />
+              </div>
+              <div className="short-about">
+                <div className="short-detail">
+                  <a href="" className="short-name">ทดลองโคลนนิ่งยูทูป</a>
+                  <div className="short-view">การดู 44.44 ล้านครั้ง</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="short">
+              <div className="short-thumb">
+                <img src="https://via.placeholder.com/270x480" alt="" />
+              </div>
+              <div className="short-about">
+                <div className="short-detail">
+                  <a href="" className="short-name">ทดลองโคลนนิ่งยูทูป</a>
+                  <div className="short-view">การดู 44.44 ล้านครั้ง</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="short">
+              <div className="short-thumb">
+                <img src="https://via.placeholder.com/270x480" alt="" />
+              </div>
+              <div className="short-about">
+                <div className="short-detail">
+                  <a href="" className="short-name">ทดลองโคลนนิ่งยูทูป</a>
+                  <div className="short-view">การดู 44.44 ล้านครั้ง</div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          
+
+        </section>
 
     </section>
   )
