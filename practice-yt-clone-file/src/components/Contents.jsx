ba20833db4308115050b204225f5947f3d246750
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-22 11:47:53 +0700

    update content

--- practice-yt-clone-file/src/components/Contents.jsx
+++ practice-yt-clone-file/src/components/Contents.jsx
@@ -31,6 +31,50 @@ function Contents() {
 
         </div>
 
+        <div className="card">
+
+          <div className="thumbnail">
+            <img src="https://via.placeholder.com/480x270" alt="" />
+          </div>
+
+          <div className="about">
+
+            <div className="chanel-img">
+              <img src="https://via.placeholder.com/36x36" alt="" />
+            </div>
+
+            <div className="chanel-detail"></div>
+
+              <div className="detail-title">ทดลองโคลนนิ่งยูทูป</div>
+              <div className="detail-name">Kawfang-ThTn</div>
+              <div className="detail-view">การดู 44.44 ล้านครั้ง • 4 วันที่ผ่านมา</div>
+
+          </div>
+
+        </div>
+
+        <div className="card">
+
+          <div className="thumbnail">
+            <img src="https://via.placeholder.com/480x270" alt="" />
+          </div>
+
+          <div className="about">
+
+            <div className="chanel-img">
+              <img src="https://via.placeholder.com/36x36" alt="" />
+            </div>
+
+            <div className="chanel-detail"></div>
+
+              <div className="detail-title">ทดลองโคลนนิ่งยูทูป</div>
+              <div className="detail-name">Kawfang-ThTn</div>
+              <div className="detail-view">การดู 44.44 ล้านครั้ง • 4 วันที่ผ่านมา</div>
+
+          </div>
+
+        </div>
+
       </section>
 
     </section>
